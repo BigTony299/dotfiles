c4dab38d993ae868133a93e62d6917f72928592a
Antoine Gagnon <antgag2.0@gmail.com>
2025-10-26 12:40:41 -0400

    search word under cursor keybind

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -64,5 +64,8 @@ vim.keymap.set("n", "<leader>o", telescope.oldfiles, {})
 vim.keymap.set("n", "<leader>g", telescope.live_grep, {})
 vim.keymap.set("v", "<leader>g", telescope.grep_string, {})
 vim.keymap.set("n", "<leader>b", telescope.buffers, {})
+vim.keymap.set("n", "<leader>s", "viw\"9y:lua require('telescope.builtin').live_grep({ default_text = vim.fn.getreg('9') })<CR><ESC>", { silent = true })
+
+-- copy paste
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
