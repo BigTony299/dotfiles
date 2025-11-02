fc785142bd7f61060bc5cd0b8401531e56b1c0f4
Antoine Gagnon <antgag2.0@gmail.com>
2025-11-02 12:38:28 -0500

    fix nvim colorscheme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -2,11 +2,17 @@ return {
 	{
 		"catppuccin/nvim",
 		opts = {
-			flavour = "latte", -- latte, frappe, macchiato, mocha
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
 		},
 		config = function()
 			vim.o.termguicolors = true
 			vim.cmd.colorscheme("catppuccin")
 		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
 	},
 }
