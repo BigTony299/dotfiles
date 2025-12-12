3ebaefd910979c9087318c9b7353ed39d06611ae
Antoine Gagnon <antgag2.0@gmail.com>
2025-12-12 08:53:21 -0500

    fix for some parser that don't highlight always by default

--- .config/nvim/lua/core/personal.lua
+++ .config/nvim/lua/core/personal.lua
@@ -1,3 +1,8 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.scrolloff = 999 -- always center cursor
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.cmd("TSEnable highlight")
+  end,
+})
