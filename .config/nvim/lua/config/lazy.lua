6bdb096be2cbc305f3177d75c14e17f536ce2a58
Antoine Gagnon <antgag2.0@gmail.com>
2025-12-13 11:42:57 -0500

    fix typo in lazy theme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -21,6 +21,6 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
-	install = { colorscheme = { "catpuccin" } },
+	install = { colorscheme = { "catppuccin" } },
 	checker = { enabled = true, notify = false },
 })
