3d7b7d518432c71ed3c68126a3340538855fac8a
Antoine Gagnon <antgag2.0@gmail.com>
2026-01-06 15:53:03 -0500

    disable lsp completion

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -14,31 +14,31 @@ vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.keymap.set("n", "<leader>w", "<C-w>w")
 
 -- snippets
-local cmp = require("cmp")
-
-require("luasnip.loaders.from_vscode").lazy_load()
-
-cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-o>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	}, {
-		{ name = "buffer" },
-	}),
-})
+-- local cmp = require("cmp")
+--
+-- require("luasnip.loaders.from_vscode").lazy_load()
+--
+-- cmp.setup({
+-- 	mapping = cmp.mapping.preset.insert({
+-- 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+-- 		["<C-f>"] = cmp.mapping.scroll_docs(4),
+-- 		["<C-o>"] = cmp.mapping.complete(),
+-- 		["<C-e>"] = cmp.mapping.abort(),
+-- 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+-- 	}),
+--
+-- 	snippet = {
+-- 		expand = function(args)
+-- 			require("luasnip").lsp_expand(args.body)
+-- 		end,
+-- 	},
+-- 	sources = cmp.config.sources({
+-- 		{ name = "nvim_lsp" },
+-- 		{ name = "luasnip" },
+-- 	}, {
+-- 		{ name = "buffer" },
+-- 	}),
+-- })
 
 -- format
 local conform = require("conform")
