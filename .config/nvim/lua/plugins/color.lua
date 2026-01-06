150f8280f9cce9f9a86411a6d41d8b2ad89cda27
Antoine Gagnon <antgag2.0@gmail.com>
2026-01-06 15:53:20 -0500

    change from catppuccin to plain colorscheme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,12 +1,27 @@
- return {
- 	{
- 		"catppuccin/nvim",
- 		config = function()
- 			require("catppuccin").setup({
- 				flavour = "mocha", -- latte, frappe, macchiato, mocha
- 			})
- 			vim.o.termguicolors = true
- 			vim.cmd.colorscheme("catppuccin")
- 		end,
- 	},
- }
+return {
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	config = function()
+	-- 		require("catppuccin").setup({
+	-- 			flavour = "mocha", -- latte, frappe, macchiato, mocha
+	-- 		})
+	-- 		vim.o.termguicolors = true
+	-- 		vim.cmd.colorscheme("catppuccin")
+	-- 	end,
+	-- },
+	{
+		"andreypopp/vim-colors-plain",
+		config = function()
+			vim.o.background = "dark"
+			vim.o.termguicolors = true
+			vim.cmd.colorscheme("plain")
+
+			local bg = "#222222"
+			local fg = "#cccccc"
+
+			vim.api.nvim_set_hl(0, "Normal", { fg = fg, bg = bg })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = bg })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = bg })
+		end,
+	},
+}
