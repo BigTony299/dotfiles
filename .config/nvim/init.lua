3353e8183899747227d94fd19513f6692d3f9468
Antoine Gagnon <antgag2.0@gmail.com>
2026-04-18 12:42:21 -0400

    migrate Comment.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,5 +5,23 @@ vim.opt.packpath:prepend(vim.fn.stdpath("config"))
 
 vim.cmd("packadd auto-save.nvim")
 
+vim.cmd("packadd Comment.nvim")
+require("Comment").setup({
+	padding = true,
+	sticky = true,
+	ignore = nil,
+	toggler = {
+		line = "<leader>cl",
+		block = "<leader>cb",
+	},
+	extra = {},
+	mappings = {
+		basic = true,
+		extra = false,
+	},
+	pre_hook = nil,
+	post_hook = nil,
+})
+
 require("core.personal")
 require("core.keymaps")
