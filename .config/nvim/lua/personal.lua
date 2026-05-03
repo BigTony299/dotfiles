b03abf9dfec0b1dcedda4e81ff7dc8d88f110191
Antoine Gagnon <antgag2.0@gmail.com>
2026-05-02 20:34:21 -0400

    disable wrapping

--- .config/nvim/lua/personal.lua
+++ .config/nvim/lua/personal.lua
@@ -1,6 +1,7 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.scrolloff = 999 -- always center cursor
+vim.opt.wrap = false
 
 vim.opt.backspace = "2"
 vim.opt.showcmd = true
@@ -12,4 +13,3 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
-
